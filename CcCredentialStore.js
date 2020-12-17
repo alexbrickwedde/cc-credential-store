e8d62f45451e90510c4e1cd545277f3d8f22718f
Alexander Brickwedde <alex_git@brickwedde.de>
2020-12-17 08:27:52 +0100

    x

---
+++ CcCredentialStore.js
@@ -0,0 +1,33 @@
+"use strict";
+
+class CcCredentialStore {
+  constructor() {
+    this.isSupported = window.PasswordCredential || window.FederatedCredential && true
+  }
+
+  store(username, password) {
+    if (!this.isSupported) {
+      return Promise.reject();
+    }
+
+    const credential = new PasswordCredential({
+      id: username,
+      password: password,
+    });
+    return navigator.credentials.store(credential);
+  }
+
+  get() {
+    return navigator.credentials.get(
+    {
+      password: true,
+      mediation: 'optional',
+    });    
+  }
+
+  manuallyLoggedOut() {
+    if (this.isSupported) {
+      navigator.credentials.preventSilentAccess();
+    }
+  }
+}
